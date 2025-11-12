599ece499db388d59513f6c750b53bda42b42247
VIHANGAGITH <vihangahansanakv@gmail.com>
2025-11-12 20:40:06 +0530

    Add deployment config

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -4,5 +4,6 @@ dotenv.config();
 
 export const ENV= {
     PORT: process.env.PORT,
-    MONGO_URI: process.env.MONGO_URI
+    MONGO_URI: process.env.MONGO_URI,
+    NODE_ENV: process.env.NODE_ENV
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,23 @@
 import express from 'express';
+import path from 'path';
 import { ENV } from './lib/env.js';
 
 const app = express();
 
+const __dirname = path.resolve();
+
+
 app.get('/health', (req, res) => {
     res.status(200).json({ message: 'API is running' });
 });
+
+// Prepare for deployment
+if (ENV.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get("/{*any}", (req, res) => { // Catch all route to handle frontend routing
+        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+    });
+}
 
 app.listen(ENV.PORT, () => console.log('Server is running on port', ENV.PORT));
 
