3b2c43b5bdda43f74f70f3c0dbdcfafc177ed8e2
VIHANGAGIT <vihangahansanakv@gmail.com>
2025-12-04 20:39:17 +0530

    Integrate Inngest to sync with Clerk auth for user management

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -5,5 +5,13 @@ dotenv.config({ quiet : true });
 export const ENV= {
     PORT: process.env.PORT,
     MONGO_URI: process.env.MONGO_URI,
-    NODE_ENV: process.env.NODE_ENV
+    NODE_ENV: process.env.NODE_ENV,
+
+    INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY,
+    INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY,
+
+    STREAM_ACCESS_KEY: process.env.STREAM_ACCESS_KEY,
+    STREAM_SECRET_KEY: process.env.STREAM_SECRET_KEY,
+
+    CLIENT_URL: process.env.CLIENT_URL,
 };
